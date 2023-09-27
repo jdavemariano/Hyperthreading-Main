ab12b6d150974b5b6d55a73b85f17d6401acf864
jdavemariano <143034233+jdavemariano@users.noreply.github.com>
2023-09-27 08:22:29 +0800

    hyperthreading_main.py

--- hyperthreading_main.py
+++ hyperthreading_main.py
@@ -3,6 +3,8 @@ import json
 
 from get_instance_ebs_details import get_instance_details, get_ebs_details
 from tag_instance_ebs import tag_target
+from instance_creation import create_instance
+from instance_termination import terminate_instance
 
 region_lookup = {
 	"USEA":"us-east-1",
@@ -14,14 +16,23 @@ region_lookup = {
 	"APAU":"ap-southeast-2"
 }
 
-instance_name = ""
-instance_id = ""
-target_ami = ""
+instance_name = "USEAPDAJ3DB23"
+instance_id = "i-074c96dc9af6a26a0"
+target_env = "dco"
+target_ami = "ami-01ec1239b9dabe1e4"
 
 target_region = region_lookup[instance_name[:4]]
 
-ec2_data = get_instance_details(instance_name, instance_id, target_region)
+ec2_data = get_instance_details(instance_name, instance_id, target_env, target_region)
 print("EC2 DETAILS DONE")
 
-ebs_data = get_ebs_details(ec2_data, target_region)
+ebs_data = get_ebs_details(ec2_data, target_env, target_region)
 print("EBS DETAILS DONE")
+
+#terminate_instance(instance_id, target_ami, target_env, target_region)
+
+#new_instance_id = create_instance(ec2_data, target_ami, target_env, target_region)
+print(new_instance_id)
+
+#tag_target(instance_name, new_instance_id, ec2_data, ebs_data, target_region, target_env)
+print("TAGS DONE")

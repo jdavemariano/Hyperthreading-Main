c8414f9e7fcdf8da23c649925e182baf33bf652b
jdavemariano <143034233+jdavemariano@users.noreply.github.com>
2023-09-27 10:58:37 +0800

    Update get_instance_ebs_details.py
    Testing without creation of file

--- get_instance_ebs_details.py
+++ get_instance_ebs_details.py
@@ -2,23 +2,21 @@ from datetime import datetime
 import boto3
 import json
 
-api_keys_file = open("api_keys.json")
-api_keys = json.load(api_keys_file)
 
-def write_json_s3(target_filename, json_data, target_folder=""):
-	target_env = "goss"
-	target_bucket = "hyper-threading-automation-logs"
-	target_date = str(datetime.today().strftime('%Y-%m-%d(%H:%M)'))
-	final_filename = f'{target_filename}({target_date}).json'
+#def write_json_s3(target_filename, json_data, target_folder=""):
+#	target_env = "goss"
+#	target_bucket = "hyper-threading-automation-logs"
+#	target_date = str(datetime.today().strftime('%Y-%m-%d(%H:%M)'))
+#	final_filename = f'{target_filename}({target_date}).json'
 
-	s3_resource = boto3.resource('s3', aws_access_key_id=api_keys[target_env]['key'], aws_secret_access_key=api_keys[target_env]['secret'])
-	folder_filename = f'{target_folder}{final_filename}'
-	s3object = s3_resource.Object(target_bucket, folder_filename)
+#	s3_resource = boto3.resource('s3', aws_access_key_id=api_keys[target_env]['key'], aws_secret_access_key=api_keys[target_env]['secret'])
+#	folder_filename = f'{target_folder}{final_filename}'
+#	s3object = s3_resource.Object(target_bucket, folder_filename)
 
-	s3object.put(Body=(bytes(json.dumps(json_data, indent=4).encode('UTF-8'))))
+#	s3object.put(Body=(bytes(json.dumps(json_data, indent=4).encode('UTF-8'))))
 
-def get_instance_details(instance_name, instance_id, target_env, target_region):
-	ec2_client = boto3.client('ec2', region_name = target_region, aws_access_key_id=api_keys[target_env]['key'], aws_secret_access_key=api_keys[target_env]['secret'])
+def get_instance_details(instance_name, instance_id, target_region):
+	ec2_client = boto3.client('ec2', region_name = target_region)
 	response = ""
 
 	try:
@@ -43,13 +41,13 @@ def get_instance_details(instance_name, instance_id, target_env, target_region):
 			instance_details['NetworkInterfaces'][counter]['Attachment']['AttachTime'] = str(instance_details['NetworkInterfaces'][counter]['Attachment']['AttachTime'])
 			counter += 1
 
-	filename = f'{instance_name}_ec2'
-	write_json_s3(filename, instance_details, "ec2-details-log/")
+#	filename = f'{instance_name}_ec2'
+#	write_json_s3(filename, instance_details, "ec2-details-log/")
 		
 	return(instance_details)
 
-def get_ebs_details(ec2_data, target_env, target_region):
-	ec2_client = boto3.client('ec2', region_name = target_region, aws_access_key_id=api_keys[target_env]['key'], aws_secret_access_key=api_keys[target_env]['secret'])
+def get_ebs_details(ec2_data, target_region):
+	ec2_client = boto3.client('ec2', region_name = target_region)
 	vol_tags = {}
 
 	for vol in ec2_data['BlockDeviceMappings']:
@@ -64,7 +62,7 @@ def get_ebs_details(ec2_data, target_env, target_region):
 				if tag['Key'] == "Name":
 					instance_name = tag['Value']
 
-			filename = f'{instance_name}_ebs'
-			write_json_s3(filename, vol_tags, "ebs-details-log/")
+#			filename = f'{instance_name}_ebs'
+#			write_json_s3(filename, vol_tags, "ebs-details-log/")
 
 	return(vol_tags)

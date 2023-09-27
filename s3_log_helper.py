4fdfc66c2b2964d23f60cef8760daab8c07f9efa
jdavemariano <143034233+jdavemariano@users.noreply.github.com>
2023-09-27 15:51:54 +0800

    Update and rename s3_log_helper_ec2.py to s3_log_helper.py
    Log helper updated

---
+++ s3_log_helper.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+from datetime import datetime   
+
+
+def write_json_s3(filename, folder):
+    log_file = open(filename)
+    log_json = log_file.read()
+    log_json = log_json.replace("'",'"')
+    log_json = json.loads(log_json)
+
+    target_bucket = "hyper-threading-automation-logs"
+    target_date = str(datetime.today().strftime('%Y-%m-%d(%H:%M:%S)'))
+    final_filename = f'{filename}({target_date}).json'
+
+    s3_resource = boto3.resource('s3')
+    folder_filename = f'{folder}/{final_filename}'
+    s3object = s3_resource.Object(target_bucket, folder_filename)
+
+    s3object.put(Body=(bytes(json.dumps(log_json, indent=4).encode('UTF-8'))))
+
+write_json_s3(ec2_details.json, ec2-details-log)
+write_json_s3(ebs_details.json, ebs-details-log)
